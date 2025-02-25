52cc6499b47cdb8ca0a74d51737a7727018fca8a
hadi <hadi@itobuz.com>
2025-02-25 16:16:28 +0530

    fix: added ref to object refrences

--- src/Models/Listing/Listing.js
+++ src/Models/Listing/Listing.js
@@ -13,6 +13,7 @@ const listingSchema = new mongoose.Schema({
     },
     sellerId: {
         type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
         required: true
     },
     isFeatured: {
@@ -36,7 +37,7 @@ const listingSchema = new mongoose.Schema({
     images: [{
         type: String,
     }],
-    address: [{type:mongoose.Schema.Types.ObjectId}]
+    address: [{type:mongoose.Schema.Types.ObjectId, ref: "addresses"}]
 }, baseOption);
 
 const Listing = mongoose.model("Listing", listingSchema);
