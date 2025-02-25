bc7a04c0f8c34657f719ca559968b1fb809e1bec
hadi <hadi@itobuz.com>
2025-02-25 18:35:51 +0530

    fix: user schema

--- Seed/createSeller.js
+++ Seed/createSeller.js
@@ -1,15 +1,15 @@
-import { Seller } from "../src/Models/UserModel.js";
+import User from "../src/Models/UserModel.js";
 import { faker } from '@faker-js/faker';
 
 const createSeller = async (n) => {
     for (let i = 0; i < n; i++) {
-        const seller = new Seller({
+        const seller = new User({
                 name: faker.internet.username(),
                 email: `seller${i}@gmail.com`,
                 password: `seller${i}`,
                 phoneNumber: 123450+i,
                 isVerified: true,
-                registrationNumber: 12340+i,
+                role:'buyer'
             })
         await seller.save()
     }
