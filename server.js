02b1b2e56bc196b774099b981d90c4ee3afe8357
Subhranil Das <subhranil@itobuz.com>
2025-02-26 18:31:19 +0530

    feat:"LYS-8 signup controller done successfully"

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import express from 'express'
-const app = express()
-const port = 3000
+import env from 'dotenv'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+import { dbConnect } from './src/config/dbConnect.js'
+import authRoute from './src/routes/authRoutes.js'
+
+env.config({})
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT || 5000
+
+app.use('/api/auth', authRoute)
+
+dbConnect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
