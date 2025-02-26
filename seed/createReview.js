77386bfe5af7525b9ba2b03440a9dfd560093379
Subhranil Das <subhranil@itobuz.com>
2025-02-27 00:20:21 +0530

    fix:LYS-15 naming issues fixed and indentations done properly

--- seed/createReview.js
+++ seed/createReview.js
@@ -1,18 +1,21 @@
-import Order from "../src/models/OrderModel.js";
-import Review from "../src/models/reviewModel.js";
 import { faker } from "@faker-js/faker";
 
-const createReview=async n=>{
-    const orders=await Order.find({});
-    for(let i=0;i<n;i++){
-        const order=orders[Math.floor(Math.random()*orders.length)];
-        const review= new Review({
+import Order from "../src/models/OrderModel.js";
+import Review from "../src/models/reviewModel.js";
+
+
+const createReview = async n => {
+    const orders = await Order.find({});
+    for (let i = 0; i < n; i++) {
+        const order = orders[Math.floor(Math.random() * orders.length)];
+        const review = new Review({
             buyerId: order.buyerId,
             adId: order.adId,
-            rating: Math.ceil(Math.random()*5),
+            rating: Math.ceil(Math.random() * 5),
             review: faker.lorem.lines(3)
         })
         await review.save();
     }
 }
+
 export default createReview;
--- seed/createSeller.js
+++ seed/createSeller.js
@@ -1,20 +1,24 @@
-import Seller from "../src/models/sellerModel.js";
-import Address from "../src/models/addressModel.js";
 import bcrypt from "bcryptjs";
 import { faker } from "@faker-js/faker";
+
+import Seller from "../src/models/sellerModel.js";
+import Address from "../src/models/addressModel.js";
+
+
 const createAddress = async () => {
-    const location=faker.location;
+    const location = faker.location;
     const address = new Address({
         line1: `${location.buildingNumber()}, ${location.street()}`,
         line2: location.streetAddress(),
         state: location.state(),
         city: location.city(),
         country: location.country(),
-        pinCode:Math.floor(Math.random()*999999)
+        pinCode: Math.floor(Math.random() * 999999)
     })
     await address.save();
     return address._id;
 }
+
 const createSeller = async n => {
     for (let i = 0; i < n; i++) {
         const seller = new Seller({
@@ -27,4 +31,5 @@ const createSeller = async n => {
         await seller.save();
     }
 }
+
 export default createSeller;
