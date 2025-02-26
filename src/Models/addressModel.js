7576a698b40a325231aa76578e39d819bcdd7e01
Subhranil Das <subhranil@itobuz.com>
2025-02-26 15:58:28 +0530

    fix: LYS-14 db design fix

---
+++ src/Models/addressModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    line1: {
+        type: String,
+        required: true
+    },
+    line2: {
+        type: String,
+        default: null
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    landMark: {
+        type: String,
+        required: false
+    },
+    pinCode: {
+        type: Number,
+        required: true
+    }
+
+}, { timestamps: true })
+
+const Address = mongoose.model("address", addressSchema);
+export default Address;
