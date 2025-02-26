cfa408450ebcac0e4cd3915bcf5e86523a7c59d1
hadi <hadi@itobuz.com>
2025-02-26 16:29:32 +0530

    feat:LYS-12 seller seed

--- seed/seed.js
+++ seed/seed.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import Buyer from "../src/Models/buyerModel.js";
 import createBuyer from "./createBuyer.js";
+import Seller from "../src/Models/sellerModel.js";
+import createSeller from "./createSeller.js";
 const main = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         await Buyer.deleteMany({});
         await createBuyer(5);
+        await Seller.deleteMany({});
+        await createSeller(5);
         await mongoose.disconnect();
         console.log('done :>> ');
 }
