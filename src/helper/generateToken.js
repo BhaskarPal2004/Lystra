740cdb11f976290f026f96367ea2b61c46b0b731
hadi <hadi@itobuz.com>
2025-02-27 12:17:25 +0530

    feat:LYS-19: helper generateToken
    the function generateToken accepts type, id, time. It create a token that expires in specified time, and had the type and id encoded in it

--- src/helper/generateToken.js
+++ src/helper/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 function generateToken(type, id, time) {
-    const payload = { type, id };//type to identify token type, id to idetify token owner
+    const payload = { type, id };//type to identify token type, id to identify token owner
     const secret = process.env.SECRET_KEY;
     const options = { expiresIn: `${time}` };
     return jwt.sign(payload, secret, options);
