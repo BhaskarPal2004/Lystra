2120f586e746fcd9a5ecbc759ce5fe65a42dd5df
bhaskar-itobuz <bhaskar@itobuz.com>
2025-02-27 14:33:06 +0530

    feat:LYS-17 sign up zod validation done

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { signup } from '../controllers/authentication/signupController.js'
+import { signup } from '../controllers/authControllers/signupController.js'
+import { signUpValidation } from '../validator/validateSignup.js'
+import {validateData} from "../middleware/validateData.js"
+
 
 const authRoute = express.Router()
 
 //all api's for authentications
-authRoute.post('/signup/:role', signup)
+authRoute.post('/signup/:role',validateData(signUpValidation),signup)
 
 export default authRoute
