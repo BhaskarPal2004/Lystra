cbdad4112b9aeb8c2f200900736ffa791fdeed3c
hadi <hadi@itobuz.com>
2025-02-27 14:34:34 +0530

    feat:LYS-30; WIP

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,10 @@ import express from 'express'
 import regenerateTokens from '../controllers/authControllers/regenerateTokens.js'
 import { verifyRefreshToken } from '../middleware/isAuthenticated.js'
 import { signup } from '../controllers/authControllers/signupController.js'
+import isVerified from '../middleware/isVerified.js'
 const authRoute = express.Router()
 
 //all api's for authentications
 authRoute.post('/signup/:role', signup)
-authRoute.get('/accessToken',verifyRefreshToken ,regenerateTokens);
+authRoute.get('/accessToken', verifyRefreshToken, isVerified, regenerateTokens);
 export default authRoute
