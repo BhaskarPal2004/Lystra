1cbecfabb886f57fef46c66c462eebfb76db67d1
hadi <hadi@itobuz.com>
2025-02-27 13:27:47 +0530

    feat:LYS-20: Encodeing role in tokens and extracting it

--- src/helper/generateToken.js
+++ src/helper/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
-function generateToken(type, id, time) {
-    const payload = { type, id };//type to identify token type, id to identify token owner
+function generateToken(type, id, time, role) {
+    const payload = { type, id, role };//type to identify token type, id to identify token owner
     const secret = process.env.SECRET_KEY;
     const options = { expiresIn: `${time}` };
     return jwt.sign(payload, secret, options);
