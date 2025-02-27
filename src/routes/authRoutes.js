fedb30ab4faba48a1429f41521cb9fe17478b110
hadi <hadi@itobuz.com>
2025-02-27 15:46:36 +0530

    feat:LYS-29: verifyUser route and controller to verify user email id

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,11 @@ import { signup } from '../controllers/authControllers/signupController.js'
 import { signUpValidation } from '../validator/validateSignup.js'
 import {validateData} from "../middleware/validateData.js"
 import regenerateTokens from '../controllers/authControllers/regenerateTokens.js'
-import { verifyRefreshToken } from '../middleware/isAuthenticated.js'
+import { verifyRefreshToken, verifyRegistrationToken } from '../middleware/isAuthenticated.js'
 import isVerified from '../middleware/isVerified.js'
+import verifyUser from '../controllers/authControllers/verifyUser.js'
 
 authRoute.post('/signup/:role',validateData(signUpValidation),signup)
 authRoute.get('/accessToken', verifyRefreshToken, isVerified, regenerateTokens);
+authRoute.post('/verifyUser/:registrationToken',verifyRegistrationToken, verifyUser );
 export default authRoute
