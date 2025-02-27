8c9c82fd044df9260aaaa9071a9dd9fce74d82b4
Aayush Kumar Gupta <aayush@itobuz.com>
2025-02-27 15:54:54 +0530

    feat: LYS-27 get all ads controller created

--- src/routes/adRoutes.js
+++ src/routes/adRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import { getAllAds } from '../controllers/adControllers/getAllAds.js';
 
 const adRoute = express.Router()
 
 //all api's of ad
 adRoute.post('')
+
+// Route to get all ads
+adRoute.get('/getAllAds', getAllAds);
 
 export default adRoute
