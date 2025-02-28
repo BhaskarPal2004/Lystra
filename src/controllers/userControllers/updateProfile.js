8e3a967617a92a79c506671acf0dd139df7fa299
hadi <hadi@itobuz.com>
2025-02-28 11:09:37 +0530

    feat:LYS-25: updateProile controller

--- src/controllers/userControllers/updateProfile.js
+++ src/controllers/userControllers/updateProfile.js
@@ -1,6 +1,30 @@
-import { SUCCESS_CODE } from "../../config/constant.js"
+import { INTERNAL_SERVER_ERROR_CODE, NOT_FOUND_CODE, SUCCESS_CODE } from "../../config/constant.js"
+import Buyer from "../../models/buyerModel.js";
+import Seller from "../../models/sellerModel.js";
+import createAddress from "../../helper/createAddress.js";
+import Address from "../../models/addressModel.js";
 
-const updateProfile=(req,res)=>{
-    res.status(SUCCESS_CODE).send("UPDATE PROFILE");
+const updateProfile = async (req, res) => {
+    try {
+        const { userId, role, name, phoneNumber, address } = req.body;
+        const User = (role === 'buyer') ? Buyer : Seller;
+        const user = await User.findById(userId);
+        user.name = (name) ?? name;
+        user.phoneNumber = (phoneNumber) ?? phoneNumber;
+        if (address) {
+            await Address.findByIdAndDelete(user.address);
+            user.address = await createAddress(address);
+        }
+        await user.save();
+        res.status(SUCCESS_CODE).send({
+            success: true,
+            message: "Profile updated successfully"
+        })
+    } catch (error) {
+        res.status(INTERNAL_SERVER_ERROR_CODE).send({
+            success: false,
+            message: error.message
+        })
+    }
 }
 export default updateProfile;
