fb1b3165222541cd4f94ad5547fa1dcf499762e3
Suchismita Singha Roy <suchismita@itobuz.com>
2025-02-28 18:41:00 +0530

    wip

--- src/controllers/adControllers/getAllAds.js
+++ src/controllers/adControllers/getAllAds.js
@@ -4,15 +4,27 @@ import Ad from "../../models/adModel.js";
 export const getAllAds = async (req, res) => {
 
   try {
-    const { searchKeyword = "", searchCategory = "" } = req.query; //either "" or some value
+    const { searchKeyword = "", searchCategory = "", sortBy } = req.query; //either "" or some value
+
+
+    //sorting
+    let sortCriteria = null
+    if (sortBy === "asc") {
+      sortCriteria = { createdAt: "asc" }
+    }
+    else {
+      sortCriteria = { createdAt: "desc" }
+    }
+
+
 
     if (searchCategory && searchKeyword === "") {
-      const filtered_notes = await Ad.find({
+      const filteredNotes = await Ad.find({
         category: new RegExp(searchCategory.trim(), 'i')
-      });
+      }).sort(sortCriteria).limit(3);
       return res.status(SUCCESS_CODE).send({
         success: true,
-        ads: filtered_notes
+        ads: filteredNotes
       });
 
     } else if (searchKeyword && searchCategory === "") {
@@ -60,67 +72,77 @@ export const getAllAds = async (req, res) => {
 
       combinedResult = [...temp]
 
+      combinedResult.sort((a, b) => {
+        const dateA = new Date(a.createdAt);
+        const dateB = new Date(b.createdAt);
+        if (sortCriteria.createdAt === "asc") {
+          return dateA - dateB;
+        } else {
+          return dateB - dateA;
+        }
+      })
+
       return res.status(SUCCESS_CODE).send({
         success: true,
         ads: combinedResult
       });
 
-    } else if(searchKeyword && searchCategory ){
+    } else if (searchKeyword && searchCategory) {
 
-      try{
-      const filteredAds = await Ad.find({
-        $and: [
-          { category: new RegExp(searchCategory.trim(), 'i') },
-          {
-            $or: [
-              { name: new RegExp(searchKeyword.trim(), 'i') },
-              { listingType: new RegExp(searchKeyword.trim(), 'i') },
-              { category: new RegExp(searchKeyword.trim(), 'i') },
-              { subCategory: new RegExp(searchKeyword.trim(), 'i') },
-              { description: new RegExp(searchKeyword.trim(), 'i') }
-            ]
-          }
-
-
-        ]
-
-      });
-      res.status(SUCCESS_CODE).send({
-        success: true,
-        ads: filteredAds
-        });
-      }
-      catch(error){
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({
-          message: error.message,
-          success: false,
-        });
-      }
-    }
-
-    else{
       try {
-        const ads = await Ad.find().populate('sellerId', 'name email');
-      res.status(SUCCESS_CODE).send({
-      success: true,
-      ads: ads
-      });
-    }
-      catch(error){
+        const filteredAds = await Ad.find({
+          $and: [
+            { category: new RegExp(searchCategory.trim(), 'i') },
+            {
+              $or: [
+                { name: new RegExp(searchKeyword.trim(), 'i') },
+                { listingType: new RegExp(searchKeyword.trim(), 'i') },
+                { category: new RegExp(searchKeyword.trim(), 'i') },
+                { subCategory: new RegExp(searchKeyword.trim(), 'i') },
+                { description: new RegExp(searchKeyword.trim(), 'i') }
+              ]
+            }
+
+
+          ]
+
+        }).sort(sortCriteria).limit(3);
+        res.status(SUCCESS_CODE).send({
+          success: true,
+          ads: filteredAds
+        });
+      }
+      catch (error) {
         res.status(INTERNAL_SERVER_ERROR_CODE).send({
           message: error.message,
           success: false,
         });
       }
     }
-  } 
 
-catch(error){
-  res.status(INTERNAL_SERVER_ERROR_CODE).send({
-    message: error.message,
-    success: false,
-  });
-}
+    else {
+      try {
+        const ads = await Ad.find().populate('sellerId', 'name email').sort(sortCriteria).limit(3);
+        res.status(SUCCESS_CODE).send({
+          success: true,
+          ads: ads
+        });
+      }
+      catch (error) {
+        res.status(INTERNAL_SERVER_ERROR_CODE).send({
+          message: error.message,
+          success: false,
+        });
+      }
+    }
+  }
+
+  catch (error) {
+    res.status(INTERNAL_SERVER_ERROR_CODE).send({
+      message: error.message,
+      success: false,
+    });
+  }
 }
 
 
