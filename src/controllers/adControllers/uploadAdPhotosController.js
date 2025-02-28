7a45d83fe615ebb8807910fa7ced41e8c081bb46
Subhranil Das <subhranil@itobuz.com>
2025-02-28 18:57:52 +0530

    wip

--- src/controllers/adControllers/uploadAdPhotosController.js
+++ src/controllers/adControllers/uploadAdPhotosController.js
@@ -1,14 +1,13 @@
-import { CREATED_CODE, INTERNAL_SERVER_ERROR_CODE, UNAUTHORIZED_CODE } from "../../config/constant.js"
+import { BAD_REQUEST_CODE, CREATED_CODE, INTERNAL_SERVER_ERROR_CODE, UNAUTHORIZED_CODE } from "../../config/constant.js"
 import Ad from "../../models/adModel.js"
 
 export const uploadAdPhotosController = async (req, res) => {
     try {
         const adId = req.params.adId
         const userId = req.userId
-        const files = req.file
-        console.log(userId)
+        const files = req.files
+
         const ad = await Ad.findById(adId).populate('sellerId')
-        console.log(ad.sellerId._id)
 
         if (ad.sellerId._id != userId) {
             return res.status(UNAUTHORIZED_CODE).json({
@@ -17,11 +16,23 @@ export const uploadAdPhotosController = async (req, res) => {
             })
         }
 
-        console.log(req.files)
+        files.forEach(async (photo) => {
+            if (ad.images.length === 6) {
+                return res.status(BAD_REQUEST_CODE).json({
+                    success: false,
+                    message: "Limit Cross"
+                })
+            }
+            const fileName = `http://localhost:3000/${photo.path}`
+            ad.images.push(fileName)
+        })
+
+        await ad.save()
 
         return res.status(CREATED_CODE).json({
             success: true,
-            message: "Pictures uploaded successfully"
+            message: "Pictures uploaded successfully",
+            images: ad.images
         })
 
     } catch (error) {
