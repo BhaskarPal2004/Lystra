e545b8dc1dc720624c3a2538ee60a8cf69f75d06
Subhranil Das <subhranil@itobuz.com>
2025-02-28 11:25:53 +0530

    feat:LYS-36 multer basic setup done for profilePicture upload

---
+++ src/middleware/multers/profilePictureMulter.js
@@ -0,0 +1,44 @@
+import multer from 'multer'
+import path from 'path'
+import { BAD_REQUEST_CODE, INTERNAL_SERVER_ERROR_CODE } from '../../config/constant'
+
+const profilePictureExtensions = ['image/jpeg', 'image/jpg', 'image/png']
+
+const validateFile = (extensions) => {
+    return (req, file, cb) => {
+        extensions.includes(file.mimetype) ? cb(null, true) : cb(new Error('Invalid file type'), false)
+    }
+}
+
+const profilePictureStorage = multer.diskStorage({
+    destination: './upload/profilePhoto',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const profilePictureUpload = multer({
+    storage: profilePictureStorage,
+    limits: {
+        fileSize: 1024 * 1024 * 10,
+        files: 1
+    },
+    fileFilter: validateFile(profilePictureExtensions)
+})
+
+export const uploadProfilePicture = (req, res, next) => {
+    try {
+        profilePictureUpload.single('profilePicture')(req, res, (error) => {
+            if (error) return res.status(BAD_REQUEST_CODE).json({
+                success: false,
+                message: error.message
+            })
+            else next()
+        })
+    } catch (error) {
+        return res.status(INTERNAL_SERVER_ERROR_CODE).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
