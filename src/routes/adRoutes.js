f4faa88ed93f8e7f93c830c4ca2f4765c756864e
bhaskar-itobuz <bhaskar@itobuz.com>
2025-02-28 12:23:28 +0530

    feat: LYS-34 deleteAllAds completed

--- src/routes/adRoutes.js
+++ src/routes/adRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { getAllAds } from '../controllers/adControllers/getAllAds.js';
 import {verifyAccessToken} from "../middleware/isAuthenticated.js"
-import { createNewAd } from '../controllers/AdControllers/createNewAd.js'
+import { createNewAd } from '../controllers/adControllers/createNewAd.js'
 import { validateData } from "../middleware/validateData.js"
 import { adSchema } from '../validator/validateAd.js'
 import { isSeller } from '../middleware/isSeller.js'
 import { getAdById } from '../controllers/adControllers/getAdById.js';
+import { deleteAllAds } from '../controllers/adControllers/deleteAllAds.js';
+import isVerified from '../middleware/isVerified.js';
 
 const adRoute = express.Router()
 
@@ -17,5 +19,8 @@ adRoute.get('/getAllAds', getAllAds);
 
 // Route to get an ad by ID
 adRoute.get('/getAdById/:id', getAdById);
+
+// Route to delete all ad
+adRoute.delete('/deleteAllAds',verifyAccessToken, isVerified, isSeller,deleteAllAds)
 
 export default adRoute
