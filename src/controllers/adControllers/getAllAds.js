bfafb30e89da79175e95c3453268250c3ef88720
Suchismita Singha Roy <suchismita@itobuz.com>
2025-03-03 17:03:44 +0530

    feat:LYS-63 price filtering implemented

--- src/controllers/adControllers/getAllAds.js
+++ src/controllers/adControllers/getAllAds.js
@@ -4,49 +4,62 @@ import Ad from "../../models/adModel.js";
 export const getAllAds = async (req, res) => {
 
   try {
-    const { searchKeyword = "", searchCategory = "", sortBy = "createdAt" , sortOrder = "asc" , searchSubCategory = ""}  = req.query;  
+    const { searchKeyword = "", searchCategory = "", sortBy = "createdAt", sortOrder = "asc", searchSubCategory = "", minPrice = 0, maxPrice = Infinity } = req.query;
+
+    let priceFilter = { $gte: 0, $lte: Infinity }
+
+    if (Number(minPrice) !== 0 || Number(maxPrice) !== Infinity) {
+      priceFilter = { $gte: Number(minPriceRange), $lte: Number(maxPriceRange) }
+    }
+
+
 
     const filteredAds = await Ad.aggregate([
-            {
-              $match: {
-                category: new RegExp(searchCategory.trim(), 'i'),
-              }
-            },
-            {
-              $match:{
-                subCategory:new RegExp(searchSubCategory.trim(), 'i'),
-              }
-            },
-            {
-              $addFields: {
-                detailsArray: { $objectToArray: "$details" }
-              }
-            },
-            {
-              $match: {
-                $or: [
-                  { name: new RegExp(searchKeyword.trim(), 'i') },
-                  { listingType: new RegExp(searchKeyword.trim(), 'i') },
-                  { category: new RegExp(searchKeyword.trim(), 'i') },
-                  { subCategory: new RegExp(searchKeyword.trim(), 'i') },
-                  { description: new RegExp(searchKeyword.trim(), 'i') },
-                  { "detailsArray.v": new RegExp(searchKeyword.trim(), 'i') }
-                  
-                ]
-              }
-            },
-            {
-              $sort: {
-                [sortBy]: sortOrder === "asc" ? 1 : -1
-              }
-            }
-          ]);
-        res.status(SUCCESS_CODE).send({
-          success: true,
-          total:filteredAds.length,
-          ads: filteredAds
-        });
+      {
+        $match: {
+          category: new RegExp(searchCategory.trim(), 'i'),
+        }
+      },
+      {
+        $match: {
+          subCategory: new RegExp(searchSubCategory.trim(), 'i'),
+        }
+      },
+      {
+        $match: {
+          price: priceFilter
+        }
+      },
+      {
+        $addFields: {
+          detailsArray: { $objectToArray: "$details" }
+        }
+      },
+      {
+        $match: {
+          $or: [
+            { name: new RegExp(searchKeyword.trim(), 'i') },
+            { listingType: new RegExp(searchKeyword.trim(), 'i') },
+            { category: new RegExp(searchKeyword.trim(), 'i') },
+            { subCategory: new RegExp(searchKeyword.trim(), 'i') },
+            { description: new RegExp(searchKeyword.trim(), 'i') },
+            { "detailsArray.v": new RegExp(searchKeyword.trim(), 'i') }
+
+          ]
+        }
+      },
+      {
+        $sort: {
+          [sortBy]: sortOrder === "asc" ? 1 : -1
+        }
       }
+    ]);
+    res.status(SUCCESS_CODE).send({
+      success: true,
+      total: filteredAds.length,
+      ads: filteredAds
+    });
+  }
 
   catch (error) {
     res.status(INTERNAL_SERVER_ERROR_CODE).send({
