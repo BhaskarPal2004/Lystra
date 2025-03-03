6283c35d5c6f56aa8a816fdd0cdc5ef44f779d24
Suchismita Singha Roy <suchismita@itobuz.com>
2025-03-03 12:31:44 +0530

    LYS-8 fix:Searching ads

--- src/controllers/adControllers/getAllAds.js
+++ src/controllers/adControllers/getAllAds.js
@@ -4,123 +4,74 @@ import Ad from "../../models/adModel.js";
 export const getAllAds = async (req, res) => {
 
   try {
-    const { searchKeyword = "", searchCategory = "" } = req.query; //either "" or some value
+    const { searchKeyword = "", searchCategory = ""}  = req.query; 
 
-    if (searchCategory && searchKeyword === "") {
-      const filtered_notes = await Ad.find({
-        category: new RegExp(searchCategory.trim(), 'i')
-      });
-      return res.status(SUCCESS_CODE).send({
-        success: true,
-        ads: filtered_notes
-      });
+   
 
-    } else if (searchKeyword && searchCategory === "") {
-      const filteredNotes = await Ad.find({
-        $or: [
-          { name: new RegExp(searchKeyword.trim(), 'i') },
-          { listingType: new RegExp(searchKeyword.trim(), 'i') },
-          { category: new RegExp(searchKeyword.trim(), 'i') },
-          { subCategory: new RegExp(searchKeyword.trim(), 'i') },
-          { description: new RegExp(searchKeyword.trim(), 'i') },
-        ]
-      });
+        const filteredAds = await Ad.find({
+          $and: [
+            { category: new RegExp(searchCategory.trim(), 'i') },
+            {
+              $or: [
+                { name: new RegExp(searchKeyword.trim(), 'i') },
+                { listingType: new RegExp(searchKeyword.trim(), 'i') },
+                { category: new RegExp(searchKeyword.trim(), 'i') },
+                { subCategory: new RegExp(searchKeyword.trim(), 'i') },
+                { description: new RegExp(searchKeyword.trim(), 'i') }
+              ]
+            }
 
-      const filteredNotesOnDetails = await Ad.aggregate([{
-        $project: {
-          name: 1,
-          sellerId: 1,
-          isFeatured: 1,
-          listingType: 1,
-          category: 1,
-          subCategory: 1,
-          description: 1,
-          images: 1,
-          price: 1,
-          details: 1,
-          createdAt: 1,
-          updatedAt: 1,
-          __v: 1,
-          address: 1,
-          performance: 1, detailsArray: { $objectToArray: "$details" }
-        }
-      }, { $unwind: "$detailsArray" }, {
-        $match: {
-          "detailsArray.v": new RegExp(searchKeyword.trim(), 'i')
-        }
-      },
-      {
-        $replaceRoot: { newRoot: "$$ROOT" }
-      }])
+          ]
 
+        })
 
-      let combinedResult = [...filteredNotes, ...filteredNotesOnDetails]
-
-      let temp = new Set(combinedResult)
-
-      combinedResult = [...temp]
-
-      return res.status(SUCCESS_CODE).send({
-        success: true,
-        ads: combinedResult
-      });
-
-    } else if(searchKeyword && searchCategory ){
-
-      try{
-      const filteredAds = await Ad.find({
-        $and: [
-          { category: new RegExp(searchCategory.trim(), 'i') },
+        const filteredAdsOnDetails = await Ad.aggregate([
           {
-            $or: [
-              { name: new RegExp(searchKeyword.trim(), 'i') },
-              { listingType: new RegExp(searchKeyword.trim(), 'i') },
-              { category: new RegExp(searchKeyword.trim(), 'i') },
-              { subCategory: new RegExp(searchKeyword.trim(), 'i') },
-              { description: new RegExp(searchKeyword.trim(), 'i') }
-            ]
+            $match: {
+              category: new RegExp(searchCategory.trim(), 'i') 
+            }
+          },
+          {
+            $addFields: {
+              detailsArray: { $objectToArray: "$details" }
+            }
+          },
+          { $unwind: "$detailsArray" },
+          {
+            $match: {
+              "detailsArray.v": new RegExp(searchKeyword.trim(), 'i')
+            }
+          },
+          {
+            $replaceRoot: { newRoot: "$$ROOT" }
           }
+        ]);
+
+      
+        const combinedResult = [...filteredAds];
+
+        const filteredAdsIds = new Set(filteredAds.map(ad => ad._id.toString()));
+
+        filteredAdsOnDetails.forEach(ad => {
+          if (!filteredAdsIds.has(ad._id.toString())) {
+            combinedResult.push(ad);
+          }
+        });
 
 
-        ]
-
-      });
-      res.status(SUCCESS_CODE).send({
-        success: true,
-        ads: filteredAds
+        res.status(SUCCESS_CODE).send({
+          success: true,
+          total:combinedResult.length,
+          ads: combinedResult
         });
       }
-      catch(error){
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({
-          message: error.message,
-          success: false,
-        });
-      }
-    }
 
-    else{
-      try {
-        const ads = await Ad.find().populate('sellerId', 'name email');
-      res.status(SUCCESS_CODE).send({
-      success: true,
-      ads: ads
-      });
-    }
-      catch(error){
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({
-          message: error.message,
-          success: false,
-        });
-      }
-    }
-  } 
-
-catch(error){
-  res.status(INTERNAL_SERVER_ERROR_CODE).send({
-    message: error.message,
-    success: false,
-  });
-}
+  catch (error) {
+    res.status(INTERNAL_SERVER_ERROR_CODE).send({
+      message: error.message,
+      success: false,
+    });
+  }
 }
 
 
