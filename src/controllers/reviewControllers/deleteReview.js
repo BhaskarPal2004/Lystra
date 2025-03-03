d0ab8d02e27cf5b0c154cd42b77641cb53705bab
Subhranil Das <subhranil@itobuz.com>
2025-03-03 16:53:34 +0530

    fix: LYS-60 review seller fixes done

--- src/controllers/reviewControllers/deleteReview.js
+++ src/controllers/reviewControllers/deleteReview.js
@@ -1,35 +1,43 @@
-import { INTERNAL_SERVER_ERROR_CODE, NOT_FOUND_CODE, SUCCESS_CODE } from "../../config/constant.js";
+import { INTERNAL_SERVER_ERROR_CODE, NOT_FOUND_CODE, SUCCESS_CODE, UNAUTHORIZED_CODE } from "../../config/constant.js";
 import Review from "../../models/reviewModel.js";
 import Seller from "../../models/sellerModel.js";
-import Ad from "../../models/adModel.js"
+
+
 const deleteReview = async (req, res) => {
     try {
-        const { reviewId } = req.params;
-        const { userId } = req;
+        const reviewId = req.params.reviewId;
+        const userId = req.userId;
+
         const review = await Review.findById(reviewId);
-        if (review) {
-            if (review.buyerId == userId) {
-                //delete from seller
-                const ad =await Ad.findById(review.adId);
-                const seller = await Seller.findById(ad.sellerId);
-                seller.reviews.remove(reviewId);
-                await seller.save();
-                //delete
-                await Review.findByIdAndDelete(reviewId);
-                res.status(SUCCESS_CODE).send({
-                    success: true,
-                    message: 'review deleted successfully'
-                })
-            } else res.status(NOT_FOUND_CODE).send({
+
+        if (!review) {
+            return res.status(NOT_FOUND_CODE).send({
                 success: false,
-                message: "review not found"
+                message: "Review not found"
             })
-        } else res.status(NOT_FOUND_CODE).send({
+        }
+
+        if (review.buyerId.toHexString() !== userId) {
+            return res.status(UNAUTHORIZED_CODE).json({
+                success: false,
+                message: "Unauthorized access"
+            })
+        }
+
+        await Review.deleteOne({ _id: reviewId })
+
+        const seller = await Seller.findById(review.sellerId)
+        seller.reviews.remove(reviewId)
+
+        await seller.save()
+
+        return res.status(SUCCESS_CODE).json({
             success: false,
-            message: "review not found"
+            message: "Review deleted successfully"
         })
+
     } catch (error) {
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({
+        return res.status(INTERNAL_SERVER_ERROR_CODE).json({
             success: false,
             message: error.message
         })
---
+++ src/controllers/reviewControllers/updateReview.js
@@ -0,0 +1,48 @@
+import { BAD_REQUEST_CODE, INTERNAL_SERVER_ERROR_CODE, NOT_FOUND_CODE, SUCCESS_CODE, UNAUTHORIZED_CODE } from "../../config/constant.js";
+import Review from "../../models/reviewModel.js";
+
+const updateReview = async (req, res) => {
+    try {
+        const reviewId = req.params.reviewId;
+        const userId = req.userId;
+        const { rating, review } = req.body;
+
+        const oldReview = await Review.findById(reviewId);
+
+        if (!oldReview)
+            return res.status(NOT_FOUND_CODE).json({
+                success: false,
+                message: "Review not found"
+            })
+
+        if (oldReview.buyerId === userId)
+            return res.status(UNAUTHORIZED_CODE).json({
+                success: false,
+                message: "un authorized access, can't edit review"
+            })
+
+        if (rating < 0 || rating > 5) {
+            return res.status(BAD_REQUEST_CODE).json({
+                success: false,
+                message: "Ratting must be between 0 to 5"
+            })
+        }
+
+        oldReview.rating = rating ? Number(rating) : oldReview.rating
+        oldReview.review = review ? review : oldReview.review
+
+        await oldReview.save();
+
+        return res.status(SUCCESS_CODE).json({
+            success: true,
+            message: 'review updated successfully'
+        })
+
+    } catch (error) {
+        return res.status(INTERNAL_SERVER_ERROR_CODE).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+export default updateReview;
