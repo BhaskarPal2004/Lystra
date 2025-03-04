ab612226a737def961ee9418e4e410b92ab654d1
hadi <hadi@itobuz.com>
2025-03-04 14:40:52 +0530

    feat: LYS-77: added reviewResponse to createReview seed

--- seed/createReview.js
+++ seed/createReview.js
@@ -15,7 +15,8 @@ const createReview = async n => {
             sellerId: ad.sellerId ,
             adId: order.adId,
             rating: Math.ceil(Math.random() * 5),
-            review: faker.lorem.lines(3)
+            review: faker.lorem.lines(3),
+            reviewResponse: faker.lorem.lines(3)
         })
         await review.save();
     }
