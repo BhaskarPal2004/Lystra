6a413f115ae6cb6d5b5573e18dc73fac06ee6c75
Suchismita Singha Roy <suchismita@itobuz.com>
2025-03-06 15:47:54 +0530

    feat:LYS-91 seller can view orders on their ad

--- src/controllers/orderControllers/saveOrdersInAd.js
+++ src/controllers/orderControllers/saveOrdersInAd.js
@@ -3,8 +3,6 @@ import Ad from "../../models/adModel.js"
 export const saveOrdersInAd = async (adId, orderId) => {
     const ad = await Ad.find({ _id: adId })
     ad[0].orders.push(orderId)
-    console.log("ad",ad)
-    console.log("orders field value",ad[0].orders)
     await ad[0].save()
    
 }
