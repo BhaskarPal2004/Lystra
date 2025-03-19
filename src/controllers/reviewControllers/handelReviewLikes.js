40b4a6a931586d96759646729f909b0276870492
bhaskar-itobuz <bhaskar@itobuz.com>
2025-03-19 11:23:40 +0530

    fix:LYS-128 reply and likes for review completed

--- src/controllers/reviewControllers/handelReviewLikes.js
+++ src/controllers/reviewControllers/handelReviewLikes.js
@@ -1,47 +1,49 @@
 import {
-  BAD_REQUEST_CODE,
   INTERNAL_SERVER_ERROR_CODE,
+  BAD_REQUEST_CODE,
   SUCCESS_CODE,
   NOT_FOUND_CODE,
 } from "../../config/constant.js";
 import Review from "../../models/reviewModel.js";
-import Reply from "../../models/replyModel.js";
 
-export const createReply = async (req, res) => {
+export const handelReviewLikes = async (req, res) => {
   try {
     const userId = req.userId;
     const reviewId = req.params.reviewId;
-    const { content } = req.body;
     const findReview = await Review.findById(reviewId);
-    
+
     if (!findReview) {
       return res.status(NOT_FOUND_CODE).send({
         success: false,
         message: "review not found",
       });
     }
-    
-    
+
     if (findReview.buyerId.toHexString() === userId) {
       return res.status(BAD_REQUEST_CODE).send({
         success: false,
-        message: "You do not reply yourself",
+        message: "You do not like yourself",
       });
     }
 
-    const newReply = await Reply.create({
-       reviewId: reviewId,
-       replierId: userId,
-       content: content,
-    });
+    if (findReview.likes.includes(userId)) {
+      const index = findReview.likes.indexOf(userId);
+      findReview.likes.splice(index, 1);
+      await findReview.save();
+      
+      return res.status(SUCCESS_CODE).send({
+        success: true,
+        message: "Like remove successfully",
+      });
+    }
 
-    await newReply.save();
+    findReview.likes.push(userId);
+    await findReview.save();
 
     return res.status(SUCCESS_CODE).send({
       success: true,
-      message: "Reply created successfully",
+      message: "Like given successfully",
     });
-
   } catch (error) {
     return res.status(INTERNAL_SERVER_ERROR_CODE).send({
       success: false,
